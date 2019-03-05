817b85c2a96d5c4aed5e25a3f71845a939a6b138
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-03-05 23:33:40 +0200

    Add weather API project

---
+++ weather-api/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("{location}")]
+        public IActionResult GetWeather(string location)
+        {
+            if (location == "Helsinki")
+            {
+                return Ok(new Weather
+                {
+                    Temperature = -15.0f,
+                    Humidity = 80.0f,
+                    WindSpeed = 5.0f
+                });
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ weather-api/Weather.cs
@@ -0,0 +1,9 @@
+namespace WeatherApi
+{
+    public class Weather
+    {
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float WindSpeed { get; set; }
+    }
+}
