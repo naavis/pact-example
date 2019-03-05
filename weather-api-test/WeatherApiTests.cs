3ddef004c5499ede3025113a86ef53e918f3edc9
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-03-06 00:57:58 +0200

    Add Pact tests for Provider (not passing yet)

---
+++ weather-api-test/WeatherApiTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using System.Net.Http;
+using WeatherApi;
+using System.Net;
+using PactNet;
+using System.Collections.Generic;
+
+namespace WeatherApi.Test
+{
+    [TestClass]
+    public class WeatherApiTests
+    {
+        private static IWebHost _webHost;
+
+        [ClassInitialize]
+        public static void Setup(TestContext _)
+        {
+            _webHost = WebHost.CreateDefaultBuilder()
+                    .UseStartup<Startup>()
+                    .Build();
+
+            _webHost.Start();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            _webHost.StopAsync().GetAwaiter().GetResult();
+            _webHost.Dispose();
+        }
+
+        [TestMethod]
+        public void CallApi()
+        {
+            var pactVerifierConfig = new PactVerifierConfig();
+            var pactVerifier = new PactVerifier(pactVerifierConfig);
+            pactVerifier
+                .ServiceProvider("Weather API", "http://localhost:5000")
+                .HonoursPactWith("Weather API Client")
+                .PactUri("../../../../pacts/weather_api_client-weather_api.json")
+                .Verify();
+        }
+    }
+}
--- weather-api/Controllers/WeatherController.cs
+++ weather-api/Controllers/WeatherController.cs
@@ -24,7 +24,7 @@ namespace WeatherApi.Controllers
             }
             else
             {
-                return NotFound();
+                return NotFound($"{location} is not a city, dummy!");
             }
         }
     }
