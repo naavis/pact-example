2794a0a4ca272c87d002a37852acc22cfdb97d49
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-03-06 23:42:03 +0200

    Add line break between properties and method

--- weather-client/Weather.cs
+++ weather-client/Weather.cs
@@ -3,6 +3,7 @@ public class Weather
     public float Temperature { get; set; }
     public float Humidity { get; set; }
     public float WindSpeed { get; set; }
+
     public override string ToString()
     {
         return $"Temperature: {Temperature}°C, Humidity: {Humidity}%, Wind speed: {WindSpeed} m/s";
